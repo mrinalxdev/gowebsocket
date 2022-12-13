f9be9c81f94abe29feb3b7e9f8a80b5fb9c694f8
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-12-13 21:56:55 +0530

    Done with The song api from Rapid Api

--- Lyricals/src/pages/Discover.jsx
+++ Lyricals/src/pages/Discover.jsx
@@ -1,18 +1,24 @@
 import {Error, Loader, SongCard} from '../components'
 import { genres} from '../assets/constants'
+import { useGetTopChartsQuery } from '../redux/services/shazamCore'
 
 const Discover = () => {
-const genreTitle = 'Pop'
+    const {data, isFetching, error} = useGetTopChartsQuery()
+    const genreTitle = 'Pop'
+
+    if(isFetching) return <Loader title="Loading Songs ....." />
+
+    if (error) return <Error />
 
     return(
         <>
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-                <h2 className='font-bold text-3x1 text-white text-left'>Discover {genreTitle}</h2>
+                <h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
                 <select 
                 onChange={() => {}}
                 value = ""
-                className=" text-grey-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+                className="text-grey-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
                 >
                     {genres.map((genre) => <option key={genre.value} value={genre.value}> {genre.title} </option>)}
 
@@ -20,7 +26,7 @@ const genreTitle = 'Pop'
             </div>
 
             <div className="flex flex-wrap sm:jsutify-start justify-center gap-8">
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+                {data?.map((song, i) => (
                     <SongCard 
                     key= {song.key}
                     song = {song}
