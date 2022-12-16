21330cfebbb74fd1c42fff5cf931df1598ddd128
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-12-16 15:08:44 +0530

    Update : Started Working with Artist Details

--- Lyricals/src/pages/SongDetails.jsx
+++ Lyricals/src/pages/SongDetails.jsx
@@ -1,3 +1,10 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {useParams} from 'react-router-dom'
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+
+
+
 const SongDetails = () => <div>SongDetails</div>;
 
 export default SongDetails;
