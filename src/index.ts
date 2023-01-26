7b7cd8801982b9eb990367c8c4e5c9cdf743fc3c
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-01-26 17:03:07 +0530

    Update : Added  Data

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid'
 
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+console.log(uuidV4())
