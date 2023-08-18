0730be305d7c5641034cf79b85d3bfdd6bc7ca2f
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-18 21:55:24 +0530

    Added New Data with React Dynamic Routing

---
+++ client/src/assets/index.js
@@ -0,0 +1,7 @@
+import HeroImage from './hero.png'
+import Facebook from './facebook.png'
+import Instagram from './instagram.png'
+
+export {
+    Facebook, Instagram, HeroImage
+}
---
+++ client/src/pages/UploadJob.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UploadJob = () => {
+  return (
+    <div>UploadJob</div>
+  )
+}
+
+export default UploadJob
